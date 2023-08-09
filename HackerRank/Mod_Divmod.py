10ba1ee83a80667482abaf86778f040655911c89
AlexGorSvami <jagger15696@gmail.com>
2023-08-09 21:43:09 +0300

    My  code is from 9.08.23

---
+++ HackerRank/Mod_Divmod.py
@@ -0,0 +1,6 @@
+num1 = int(input())
+num2 = int(input())
+result = divmod(num1, num2)
+print(result[0])
+print(result[1])
+print(result)
--- Haskell/recursive.hs
+++ Haskell/recursive.hs
@@ -31,3 +31,10 @@ factorial1 n | n == 0 = 1
 neg_fibonachi n | n > 1 = neg_fibonachi(n - 1) + neg_fibonachi(n - 2)
                 | n < 0 = neg_fibonachi (n + 2) + neg_fibonachi( n + 1)
                 | otherwise = n
+
+fibonacci' :: Integer -> Integer
+fibonacci' 0 = 0
+fibonacci' 1 = 1
+fibonacci' (-1) = 1
+fibonacci' (-2) = (-1)
+fibonacci' n = if n >= 0 then fibonacci' (n - 1) + fibonacci' (n - 2) else fibonacci' (n + 2) - fibonacci' (n + 1)
