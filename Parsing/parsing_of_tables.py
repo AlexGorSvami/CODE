401c963940f3e7391ddf9d1ce7e340520eb3ec04
AlexGorSvami <jagger15696@gmail.com>
2023-09-15 14:19:43 +0300

    My code is from 15.09.23

---
+++ Parsing/parsing_of_tables.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://parsinger.ru/table/1/index.html'
+response = requests.get(url)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+nums = [float(i.text) for i in soup.find_all('td')]
+print(sum(set(nums)))
+
+print('-------------------------2--------------------')
+url = 'https://parsinger.ru/table/2/index.html'
+response = requests.get(url)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+# nums = [float(i.text) for i in soup.find_all('td')]
+# res = 0
+# for i in range(0,len(nums),15):
+#     res += nums[i]
+# print(res)
+td = [float(x.find('td').text) for x in soup.find_all('tr') if x.find('td')]
+print(sum(td))
+print('---------------------------------------3-----------------------------')
+url = 'https://parsinger.ru/table/3/index.html'
+response = requests.get(url)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+td = [float(i.text) for i in soup.find_all('b')]
+print(sum(td))
+
+print('----------------------------4-----------------------------')
+url = 'https://parsinger.ru/table/4/index.html'
+response = requests.get(url)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+res = [float(i.text) for i in soup.find_all('td', class_='green')]
+print(sum(res))
+
+print('----------------------------5------------------------------')
+url = 'https://parsinger.ru/table/5/index.html'
+response = requests.get(url)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+res = [float(i.text) for i in soup.find_all('td', class_='orange')]
+blue = [float(i.text) for i in soup.select('td:last-child')]
+print(sum(map(lambda x: x[0]*x[1], zip(res, blue))))
