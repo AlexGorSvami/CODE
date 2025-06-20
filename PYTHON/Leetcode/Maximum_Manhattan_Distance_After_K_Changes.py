e045e7b65e0ffe3b726244ce47f9227fe9aaf60e
AlexGorSvami <jagger15696@gmail.com>
2025-06-20 16:27:12 +0500

    daily leetcode decision

---
+++ PYTHON/Leetcode/Maximum_Manhattan_Distance_After_K_Changes.py
@@ -0,0 +1,21 @@
+def maxDistance(s: str, k: int) -> int:
+    answer = 0
+    north = south = east = west = 0
+    for i in range(len(s)):
+        c = s[i]
+        if c == 'N':
+            north += 1
+        elif c == 'S':
+            south += 1 
+        elif c == 'E':
+            east += 1 
+        elif c == 'W'"
+        west += 1 
+
+        x = abs(north - south)
+        y = abs(east - west)
+        MD = x + y
+        dis = MD + min(2 * k, i + 1 - MD)
+        answer = max(answer, dis)
+
+    return answer 
