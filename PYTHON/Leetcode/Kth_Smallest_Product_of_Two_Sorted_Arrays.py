78af4b4946d6fa14632920139ec19d6c52e02f28
AlexGorSvami <jagger15696@gmail.com>
2025-06-25 05:57:14 +0500

    daily leetcode decision

---
+++ PYTHON/Leetcode/Kth_Smallest_Product_of_Two_Sorted_Arrays.py
@@ -0,0 +1,31 @@
+def kthSmallestProduct(nums1:list, nums2: list, k: int) -> int:
+    n,m = len(nums1), len(nums2)
+    a1, a2 = [-i for i in nums1 if a < 0][::-1], [i for i in nums1 if i >= 0]
+    b1, b2 = [-i for i in nums2 if i < 0][::-1], [i for i in nums2 if i >= 0]
+
+    negative = len(a1) * len(b1) + len(a2) * len(b2)
+    if k > negative:
+        k -= negative
+        s = 1 
+    else:
+        k = negative - k + 1 
+        b1, b2 = b2, b1 
+        s = -1
+
+    def cnt(nums1, nums2, x):
+        result = 0 
+        j = len(nums2)-1
+        for i in range(len(nums1)):
+            while j >= 0 and nums1[i] * nums2[j] > x:
+                j -= 1 
+            result += j+1 
+        return result 
+
+    left, right = 0, 10**10
+    while left < right:
+        middle = (left + right) // 2 
+        if cnt(a1, b1, middle) + cnt(a2, b2, mid) >= k:
+            right = middle 
+        else:
+            left = middle + 1 
+    return left * s 
