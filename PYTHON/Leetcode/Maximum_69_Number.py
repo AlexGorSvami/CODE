0450d5b791e95c3bf3321835f4e8ae2f9986ca65
AlexGorSvami <jagger15696@gmail.com>
2025-08-16 12:46:17 +0500

    daily leetcode decision

---
+++ PYTHON/Leetcode/Maximum_69_Number.py
@@ -0,0 +1,7 @@
+def meximum69Number(num: int) -> int:
+    num = list(str(num))
+    for i, n in enumerate(num):
+        if n == '6':
+            num[i] = '9'
+            break
+    return int(''.join(num))
