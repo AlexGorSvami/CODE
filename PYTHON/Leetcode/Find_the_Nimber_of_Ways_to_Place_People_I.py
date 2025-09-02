f68638edbbf8c1eb84a44dd500c8ded6b9a6e142
AlexGorSvami <jagger15696@gmail.com>
2025-09-02 18:27:25 +0500

    daily leetcode decision

--- PYTHON/Leetcode/Find_the_Nimber_of_Ways_to_Place_People_I.py
+++ PYTHON/Leetcode/Find_the_Nimber_of_Ways_to_Place_People_I.py
@@ -3,7 +3,7 @@ def numberOfPairs(points: list) -> int:
     points.sort(key=lambda x: (x[0], -x[1]))
     for i in range(len(points)):
         _, y1 = points[i]
-        y_max = float('inf')
+        y_max = float('-inf')
         for j in range(i+1, len(points)):
             _, y2 = points[j]
             if y1 > y2 and y2 > y_max:
