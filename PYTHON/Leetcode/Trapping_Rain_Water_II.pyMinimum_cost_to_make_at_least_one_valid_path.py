8c8facfb7407267df02dde8fd55b5f3b0e75c0f6
AlexGorSvami <jagger15696@gmail.com>
2025-10-03 14:58:54 +0500

    daily leetcode decision

---
+++ PYTHON/Leetcode/Trapping_Rain_Water_II.pyMinimum_cost_to_make_at_least_one_valid_path.py
@@ -0,0 +1,27 @@
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        
+        min_heap = []
+        rows, cols = len(heightMap), len(heightMap[0])
+        visit = [[False]*cols for _ in range(rows)]
+
+        for first_ind in range(rows):
+            for second_ind in range(cols):
+                if first_ind in (0, rows-1) or second_ind in (0, cols - 1):
+                    heapq.heappush(min_heap, (heightMap[first_ind][second_ind], first_ind, second_ind))
+                    visit[first_ind][second_ind] = True
+
+        result = 0
+        min_boundary_height = 0
+        while min_heap:
+            height, first_ind, second_ind = heapq.heappop(min_heap)
+            min_boundary_height = max(min_boundary_height, height)
+            for x,y in ((1,0), (-1,0), (0,1), (0,-1)):
+                right, center = first_ind + x, second_ind + y
+                if 0 <= right < rows and 0 <= center < cols and not visit[right][center]:
+                    visit[right][center] = True 
+                    heapq.heappush(min_heap, (heightMap[right][center], right, center))
+                    if heightMap[right][center] < min_boundary_height:
+                        result += min_boundary_height - heightMap[right][center]
+
+        return result 
