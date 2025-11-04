651b9fbf8d815238a66a9f8db92b0da9cd57d9d2
AlexGorSvami <jagger15696@gmail.com>
2025-11-04 08:10:58 +0500

    daily leetcode decision

--- PYTHON/Leetcode/Find_X-Sum_of_All_K-Long_Subarrays_I.py
+++ PYTHON/Leetcode/Find_X-Sum_of_All_K-Long_Subarrays_I.py
@@ -4,7 +4,7 @@ def findXSum(nums: list, k: int, x: int) -> list:
     result = []
     for i in range(l - k + 1):
         s = nums[i:i + k]
-        ls = sorted([(i, j) for i, j in Counter(s).items()], reverse = True)
+        ls = sorted([(j, i) for i, j in Counter(s).items()], reverse = True)
         if len(ls) < x:
             result.append(sum(s))
         else:
