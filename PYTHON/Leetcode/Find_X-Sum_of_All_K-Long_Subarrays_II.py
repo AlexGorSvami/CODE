defa97ae69d2ef5a2d69781ae677242f81cc12b9
AlexGorSvami <jagger15696@gmail.com>
2025-11-05 08:08:36 +0500

    daily leetcode decision

---
+++ PYTHON/Leetcode/Find_X-Sum_of_All_K-Long_Subarrays_II.py
@@ -0,0 +1,50 @@
+def findXSum(nums: list, k: int, x: int) -> list:
+        large_part = SortedList()
+        small_part = SortedList()
+        cnt = defaultdict(int)
+        x_sum = 0
+        n = len(nums)
+        def add(v: int):
+            if cnt[v] == 0:
+                return
+            p = (cnt[v], v)
+            if large_part and p > large_part[0]:
+                nonlocal x_sum
+                x_sum += p[0] * p[1]
+                large_part.add(p)
+            else:
+                small_part.add(p)
+
+        def remove(v: int):
+            if cnt[v] == 0:
+                return
+            p = (cnt[v], v)
+            if p in large_part:
+                nonlocal x_sum
+                x_sum -= p[0] * p[1]
+                large_part.remove(p)
+            else:
+                small_part.remove(p)
+
+        ans = [0] * (n - k + 1)
+        for i, v in enumerate(nums):
+            remove(v)
+            cnt[v] += 1
+            add(v)
+            j = i - k + 1
+            if j < 0:
+                continue
+            while small_part and len(large_part) < x:
+                p = small_part.pop()
+                large_part.add(p)
+                x_sum += p[0] * p[1]
+            while len(large_part) > x:
+                p = large_part.pop(0)
+                x_sum -= p[0] * p[1]
+                small_part.add(p)
+            ans[j] = x_sum
+
+            remove(nums[j])
+            cnt[nums[j]] -= 1
+            add(nums[j])
+        return ans
