046bb259d52eb262f47cdb5ec4d73b5409612737
AlexGorSvami <jagger15696@gmail.com>
2025-12-01 20:24:28 +0500

    daily leetcode decision

---
+++ PYTHON/Leetcode/Maximum_Running_Time_of_N_Computers.py
@@ -0,0 +1,25 @@
+def maxRunTime(n: int, batteries: list) -> int:
+        B = len(batteries)
+        if B < n:
+            return 0
+        if B == n:
+            return min(batteries)
+        batteries.sort()
+        presum = [0] * (len(batteries) + 1)
+        for i in range(B):
+            presum[i + 1] = batteries[i] + presum[i]
+
+        def test(minute):
+            i = bisect.bisect_left(batteries, minute)
+            if i == 0:
+                return True
+            return (n - (B - i)) * minute <= presum[i]
+
+        l, r = 0, presum[-1] // n + 1
+        while l < r:
+            mid = (l + r) // 2
+            if test(mid):
+                l = mid + 1
+            else:
+                r = mid
+        return l - 1
