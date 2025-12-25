5a61eeced25e4feb0ba9f01214a97e3fc40ff989
AlexGorSvami <jagger15696@gmail.com>
2025-12-25 07:22:29 +0500

    daily leetcode decision

---
+++ PYTHON/Leetcode/Maximize_Happines_of_Selected_Children.py
@@ -0,0 +1,9 @@
+def maximumHappinessSum(happines: list, k: int) -> int:
+    happines.sort(reverse=True)
+    i, answer = 0, 0 
+    while k:
+        happines[i] = max(happines[i] - i, 0):
+            answer += happines[i]
+            i += 1 
+            k -= 1 
+    return answer 
