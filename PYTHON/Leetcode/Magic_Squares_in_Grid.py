976d48223e5250181cb3f4988e49e280e0908b30
AlexGorSvami <jagger15696@gmail.com>
2025-12-30 10:44:31 +0500

    daily leetcode decision

---
+++ PYTHON/Leetcode/Magic_Squares_in_Grid.py
@@ -0,0 +1,23 @@
+def numMagicSquaresInside(grid: list) -> int:
+    n = len(grid)
+    m = len(grid[0])
+    if n < 3 or m < 3:
+        return 0 
+    count = 0 
+    magic = [
+            [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+            [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+            [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+            [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+            [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+            [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+            [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+            [[8, 1, 6], [3, 5, 7], [4, 9, 2]]
+        ]
+    for r_start in range(n - 2):
+        for c_start in range(m - 2):
+            sub = [grid[r_start + i][c_start:c_start + 3] for i in range(3)]
+            if sub in magic:
+                count += 1 
+
+    return count 
