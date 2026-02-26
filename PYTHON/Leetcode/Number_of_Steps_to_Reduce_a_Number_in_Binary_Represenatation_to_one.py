86c293da3d14cf77e23ceb1529529a6f45b5c1b1
AlexGorSvami <jagger15696@gmail.com>
2026-02-26 10:14:30 +0500

    Training code

---
+++ PYTHON/Leetcode/Number_of_Steps_to_Reduce_a_Number_in_Binary_Represenatation_to_one.py
@@ -0,0 +1,10 @@
+def numSteps(s: str) -> int:
+    result = 0 
+    num = int(s, 2)
+    while num != 1:
+        if num % 2:
+            num += 1 
+        else:
+            num /= 2 
+        result += 1
+    return result
