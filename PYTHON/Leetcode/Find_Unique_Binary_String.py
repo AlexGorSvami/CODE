1e8c93a2349a870a7fe37e3f24cce3c790f7522f
AlexGorSvami <jagger15696@gmail.com>
2026-03-08 08:18:03 +0500

    Leetcode daily decision

--- PYTHON/Leetcode/Find_Unique_Binary_String.py
+++ PYTHON/Leetcode/Find_Unique_Binary_String.py
@@ -1,7 +1,12 @@
 def findDifferentBinaryString(nums: list) -> str:
     return ''.join(str(int(j[i]=='0')) for i, j in enumerate(nums))
 
-
+def findDifferentBinaryString1(nums: list) -> str:
+    n = len(nums[0])
+    for i in range(2**n):
+        i = format(i, '0' + str(n) + 'b')
+        if i not in nums:
+            return i
 print(findDifferentBinaryString(['01', '10']))
 print(findDifferentBinaryString(['00', '01']))
 print(findDifferentBinaryString(['111', '011', '001']))
