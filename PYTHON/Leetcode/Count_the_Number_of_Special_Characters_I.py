6f6a476ccb2dad94fe78cbb9a861d85122f2ee8a
AlexGorSvami <jagger15696@gmail.com>
2026-05-26 18:36:32 +0500

    Fix my bug

--- PYTHON/Leetcode/Count_the_Number_of_Special_Characters_I.py
+++ PYTHON/Leetcode/Count_the_Number_of_Special_Characters_I.py
@@ -1,5 +1,5 @@
 class Solution: 
     def numberOfSpecialChars(self, word: str) -> int:
-        lowers = set(i for i in word.lower())
-        uppers = set(i for i in word.upper())
+        lowers = set(i for i in word if word.islower())
+        uppers = set(i.lower() for i in word if word.isupper())
         return len(lowers & uppers)
