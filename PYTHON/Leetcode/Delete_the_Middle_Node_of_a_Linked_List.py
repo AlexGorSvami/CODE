ca59e694bfaef7658a64cf9f5b2363cb85c7efd7
AlexGorSvami <jagger15696@gmail.com>
2026-06-15 20:12:21 +0500

    Leetcode daily decision

---
+++ PYTHON/Leetcode/Delete_the_Middle_Node_of_a_Linked_List.py
@@ -0,0 +1,8 @@
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next: return None 
+        slow, fast = head, head.next.next 
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next 
+        slow.next = slow.next.next 
+        return head 
